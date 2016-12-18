658a72fa3ff84439267cd3a297e14ce89395b9a9
piotr <piothermail@gmail.com>
2016-12-18 21:52:35 +0100

    now animating using unit_module

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import pygame
 
-import drawing_module
+import sprite_module 
+import unit_module
 
 
+unit_types = unit_module.read_units_xml("Data/units.xml")
+blobby_type = unit_types[0]
+blobby = unit_module.UnitInstance(blobby_type, (500,500))
 screen = pygame.display.set_mode((1000, 1000)) #Create the screen
-blob = drawing_module.UnitSprite("./Data/blob.png", (100, 100), None)
 
 clock = pygame.time.Clock() 
-i = 0
 while True:
     screen.fill((0, 0, 0))
-    blob.draw_idle((100, 100), screen, 1/60.)
+    blobby.animate(screen, 1/60.)
     pygame.display.flip()
     clock.tick(60)
-    i += 1
